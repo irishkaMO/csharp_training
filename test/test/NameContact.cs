02bc6c231455480c3fa22b708342f08f270a1db7
IrinaKorteleva <irkakort@gmail.com>
2018-08-01 09:20:48 -0400

    TestContact

---
+++ test/test/NameContact.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class NameContact
+    {
+        private string firstname;
+        private string lastname;
+        private string middlename;
+        public NameContact(string firstname, string middlename, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.middlename = middlename;
+        }
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string Middlename
+        {
+            get
+            {
+                return middlename;
+            }
+            set
+            {
+                middlename = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname= value;
+            }
+        }
+       
+
+
+    }
+}
---
+++ test/test/СontactCreationTest.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class KontactCreationTest
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.BrowserExecutableLocation = @"C:\Users\User\Documents\firefox-45.7.0esr.win64.sdk\firefox-sdk\bin\firefox.exe";
+            options.UseLegacyImplementation = true;
+            driver = new FirefoxDriver(options);
+            baseURL = "http://localhost/";
+            verificationErrors = new StringBuilder();
+            
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void ContactCreationTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin","secret"));
+            GoToAddNewContacPage();
+            NameСontactForm (new NameContact ("Irina",  "sys", "Korteleva"));
+            NiсkСontact();
+            AddressContact();
+            PhoneContact();
+            DateOfBirthday();
+            CreateContact();
+            Logout();
+        }
+
+        private void CreateContact()
+        {
+           
+            driver.FindElement(By.Name("submit")).Click();
+
+        }
+
+        private void Logout()
+        {
+         
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void DateOfBirthday()
+        {
+          
+            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText("8");
+            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText("November");
+            driver.FindElement(By.Name("byear")).Clear();
+            driver.FindElement(By.Name("byear")).SendKeys("1983");
+        }
+
+        private void PhoneContact()
+        {
+           
+            driver.FindElement(By.Name("home")).Clear();
+            driver.FindElement(By.Name("home")).SendKeys("7777777777");
+        }
+
+        private void AddressContact()
+        {
+            
+            driver.FindElement(By.Name("address")).Clear();
+            driver.FindElement(By.Name("address")).SendKeys("grodno");
+        }
+
+        private void NiсkСontact()
+        {
+            
+            driver.FindElement(By.Name("nickname")).Clear();
+            driver.FindElement(By.Name("nickname")).SendKeys("kotik");
+        }
+
+        private void NameСontactForm(NameContact FIO)
+        {
+            
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(FIO.Firstname);
+            driver.FindElement(By.Name("middlename")).Clear();
+            driver.FindElement(By.Name("middlename")).SendKeys(FIO.Middlename);
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(FIO.Lastname);
+        }
+
+        private void GoToAddNewContacPage()
+        {
+          
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        private void Login(AccountData log)
+        {
+           
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(log.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(log.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+        
+            driver.Navigate().GoToUrl(baseURL + "/addressbook/");
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
