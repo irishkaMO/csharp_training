966036f9052a8194bf3642a76f1e58da10c54c32
IrinaKorteleva <irkakort@gmail.com>
2018-09-16 12:18:32 -0400

    Task 16 last

---
+++ test/test/tests/ContactTestBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    class ContactTestBase:AuthTestBase
+    {
+
+        [TearDown]
+        public void CompareContactUI_DB()
+        {
+            if (PERFORM_LONG_UI_CHEKS)
+            {
+                List<ContactData> fromUI = app.Contact.GetContactList();
+                List<ContactData> fromDB = ContactData.GetAll();
+                fromDB.Sort();
+                fromUI.Sort();
+                Assert.AreEqual(fromUI, fromDB);
+            }
+        }
+    }
+     
+}
